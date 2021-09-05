5ea3e1295aa79ee7ddda29347ce86d1235cb493e
kovalyov2014 <88457883+kovalyov2014@users.noreply.github.com>
2021-09-05 23:53:33 +0300

    Homework7

---
+++ Student.java
@@ -0,0 +1,21 @@
+//package ua.kovalyov.hw7;
+//
+//public class Student {
+//    String firstName,lastName,group;
+//    double avgMark;
+//
+//    public Student(String firstName, String lastName, String group, double avgMark) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.group = group;
+//        this.avgMark = avgMark;
+//    }
+//
+//    public Student() {
+//    }
+//
+//
+//    public class getScholarship(){
+//        return avgMark == 5 ? 100 : 80;
+//    }
+//}
