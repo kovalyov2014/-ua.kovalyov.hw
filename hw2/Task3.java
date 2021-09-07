e1b62dcdf0d3f331c4f5714a29bd00fc3ae4c7f4
kovalyov2014 <88457883+kovalyov2014@users.noreply.github.com>
2021-09-08 00:27:28 +0300

    Hw2

---
+++ hw2/Task3.java
@@ -0,0 +1,16 @@
+//package ua.kovalyov.hw2;
+//
+//public class Task3 {
+//    public static void main(String[] args) {
+//        evenOdd(0);
+//        evenOdd(1);
+//    }
+//
+//    public static void evenOdd(int x) {
+//        if (x % 2 == 0) {
+//            System.out.println("Число: " + x + " - четное");
+//        } else {
+//            System.out.println("Число: " + x + " - нечетное");
+//        }
+//    }
+//}
